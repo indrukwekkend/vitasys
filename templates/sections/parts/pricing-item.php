1b1eaffdf39f1fc46e11168b5599bc7e21f6adbd
odil-io <github@odil.io>
2018-01-18 17:15:43 +0100

    Fix: get_sub_field() ipv get_field()

--- templates/sections/parts/pricing-item.php
+++ templates/sections/parts/pricing-item.php
@@ -1,5 +1,5 @@
 <?php use Roots\Sage\Extras; ?>
-<?php // Version: v2.0.0 ?>
+<?php // Version: v2.0.1 ?>
 
 <?php if( get_sub_field('image') ): ?>
 	<div class="row">
@@ -15,16 +15,16 @@
 
 		<div class="row align-items-center pt-1 pb-3">
 
-      <?php if( get_field('number') ): ?>
+      <?php if( get_sub_field('number') ): ?>
         <div class="col-1 text-center">
           <strong><?php the_sub_field('number'); ?>.</strong>
         </div>
       <?php endif; ?>
 
-      <?php if( get_field('label') or get_sub_field('description') ): ?>
+      <?php if( get_sub_field('label') or get_sub_field('description') ): ?>
         <div class="col col-md-9">
 
-          <?php if( get_field('label') ): ?>
+          <?php if( get_sub_field('label') ): ?>
             <div class="row">
               <div class="col">
                 <strong><?php the_sub_field('label'); ?></strong>
