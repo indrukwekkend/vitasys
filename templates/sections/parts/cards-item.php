4669079f7a1a6e7f750e9f6214acc37ff932ed72
odil-io <github@odil.io>
2018-01-19 14:36:26 +0100

    Cards: verbeterd

--- templates/sections/parts/cards-item.php
+++ templates/sections/parts/cards-item.php
@@ -1,15 +1,24 @@
-<?php // Version: v1.0.0 ?>
-
-
 <div class="card card-shadow">
 
-  <div class="card-body">
+  <div class="card-body pt-5 text-center">
 
-    <h5 class="card-title"><?php the_sub_field('title');?></h5>
+    <?php if( get_sub_field('title') ): ?>
+      <h5 class="card-title mb-4">
+        <?php the_sub_field('title'); ?>
+      </h5>
+    <?php endif; ?>
 
-    <h6 class="card-subtitle mb-2 text-muted"><?php the_sub_field('subtitle');?></h6>
+    <?php if( get_sub_field('subtitle') ): ?>
+      <h6 class="card-subtitle text-muted">
+        <?php the_sub_field('subtitle'); ?>
+      </h6>
+    <?php endif; ?>
 
-    <div class="card-text"><?php the_sub_field('content');?></div>
+    <?php if( get_sub_field('content') ): ?>
+      <div class="card-text">
+        <?php the_sub_field('content'); ?>
+      </div>
+    <?php endif; ?>
 
   </div>
 
