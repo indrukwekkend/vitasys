509e69363ec9e8c37c27b6049b52ac74a0f9694d
odil-io <github@odil.io>
2018-01-19 09:24:16 +0100

    Fix: columnized output

--- templates/sections/parts/columns-item.php
+++ templates/sections/parts/columns-item.php
@@ -1,13 +1,17 @@
-<?php // Version: v2.0.0 ?>
+<?php // Version: v2.0.1 ?>
 
 <div class="col">
 
   <div class="row">
-    <h1 class="display-4"><?php the_sub_field('title'); ?></h1>
+    <div class="col">
+      <h1><?php the_sub_field('title'); ?></h1>
+    </div>
   </div>
 
   <div class="row">
-    <?php the_sub_field('content'); ?>
+    <div class="col">
+      <?php the_sub_field('content'); ?>
+    </div>
   </div>
 
 </div>
