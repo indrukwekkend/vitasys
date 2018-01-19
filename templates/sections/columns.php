198b9a0e4978250289114d403b2910d1fbd77221
odil-io <github@odil.io>
2018-01-19 11:13:59 +0100

    Columns: style aanpassingen

--- templates/sections/columns.php
+++ templates/sections/columns.php
@@ -1,14 +1,20 @@
-<?php // Version: v2.0.0 ?>
+<?php // Version: v2.0.1 ?>
 
-<section id="<?= strtolower( get_sub_field('title') ); ?>" class="columns">
+<section id="<?= strtolower( str_replace( ' ','-', get_sub_field('title') ) ); ?>" class="columns">
 
-	<div class="container py-5">
+	<div class="container pb-5">
 
-		<div class="row">
-			<div class="col-12">
-				<h1 class="display-4 mb-4 text-center"><?php the_sub_field('title'); ?></h1>
-			</div>
-		</div>
+		<div class="row py-5 text-center">
+
+      <div class="col-12">
+
+      	<h1 class="mb-3 display-4">
+          <?php the_sub_field('title'); ?>
+        </h1>
+
+      </div>
+
+  	</div>
 
 		<?php if( have_rows('items') ): ?>
 
