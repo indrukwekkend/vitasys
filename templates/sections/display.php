417af712155b78417995d8c6f55459d82d8af904
odil-io <github@odil.io>
2018-01-19 11:13:40 +0100

    Display: style aanpassingen

--- templates/sections/display.php
+++ templates/sections/display.php
@@ -1,14 +1,14 @@
-<?php // Version: v2.0.0 ?>
+<?php // Version: v2.0.1 ?>
 
-<section class="display">
+<section id="<?= strtolower( str_replace( ' ','-', get_sub_field('title') ) ); ?>" class="display">
 
   <div class="container">
 
-    <div class="row py-3 text-center">
+    <div class="row py-5 text-center">
 
       <div class="col-12">
 
-        <h1 class="<?php the_sub_field('type'); ?>">
+        <h1 class="mb-3 <?php the_sub_field('type'); ?>">
           <?php the_sub_field('title'); ?>
         </h1>
 
