68d31fa297ab7c609c1498a6852e3841c826fc5d
odil-io <github@odil.io>
2018-01-19 15:41:45 +0100

    Visual: visuale modus toegevoegd
    Er kan gekozen worden tussen een vrijstaande afbeelding en een
    afbeelding over de breedte van de pagina(container)

--- lib/fields/visual.php
+++ lib/fields/visual.php
@@ -1,15 +1,16 @@
 <?php
 /*
-* Title:           Visual layout
-* Description:     Displays a row with a background image
-* Version:         v2.0.0
-*
-* Field Map:
-* image (image url)
-*
-* Files:
-* templates/sections/visual.php
-*/
+ * Title:           Visual layout
+ * Description:     Displays a row with a background image
+ * Version:         v2.0.1
+ *
+ * Field Map:
+ * image (image url)
+ * toggle (button_group)
+ *
+ * Files:
+ * templates/sections/visual.php
+ */
 
 add_action('acf/init', function(){
 
@@ -18,13 +19,28 @@ add_action('acf/init', function(){
     'title' => __('Afbeelding'),
     'fields' => array(
       array(
+        'key' => 'field_toggle',
+        'label' => 'Visuele modus',
+        'name' => 'toggle',
+        'type' => 'button_group',
+        'instructions' => 'Toon de afbeelding vrijstaand of over de volledige breedte van de pagina',
+        'choices' => array(
+          'contained' => 'Vrijstaand',
+          'full' => 'Pagina breedte',
+        ),
+        'allow_null' => 0,
+        'default_value' => 'full',
+        'layout' => 'horizontal',
+        'return_format' => 'value',
+      ),
+      array(
         'key' => 'field_image',
         'label' => __('Afbeelding'),
         'name' => 'image',
         'type' => 'image',
         'instructions' => __('Kies een afbeelding'),
         'return_format' => 'url',
-        'preview_size' => 'thumbnail',
+        'preview_size' => 'large',
         'library' => 'all',
       ),
     ),
--- templates/sections/visual.php
+++ templates/sections/visual.php
@@ -1,7 +1,22 @@
-<section class="visual container">
-  <div class="row py-3">
-    <div class="col-12 text-center">
-      <img class="img-fluid" src="<?php the_sub_field('image'); ?>"/>
+<?php if( get_sub_field('toggle') == "full" ): ?>
+
+  <section class="visual container-fluid" style="background-image:url(<?php the_sub_field('image'); ?>);background-size:cover;background-position:center; min-height:480px; ">
+  </section>
+
+<?php else: ?>
+
+  <section class="visual container">
+
+    <div class="row py-3">
+
+      <div class="col-12 text-center">
+
+        <img class="img-fluid" src="<?php the_sub_field('image'); ?>"/>
+
+      </div>
+
     </div>
-  </div>
-</section>
+
+  </section>
+
+<?php endif; ?>
